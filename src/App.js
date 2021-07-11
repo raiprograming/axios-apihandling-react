3c876361dbca4e8f3ea5e66d4cbc51390efb3a4d
raiprograming <raishivam2304@gmail.com>
2021-07-11 21:36:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from "react";
+import { Container,Row,Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Axios from "axios";
+import MyCard from "./Card";
+
+
 
 function App() {
+
+  
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async ()=>{
+    const { data }=await Axios.get("https://randomuser.me/api/");
+
+    console.log("response",data);
+     
+    const details=data.results[0];
+    setDetails(details);
+
+  }
+
+  useEffect(()=>{
+    fetchDetails();
+  },[])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container className="p-4 bg-primary App" fluid>
+
+    <Row>
+      <Col md={4}
+       className="offset-md-4 mt-4">
+         <MyCard  details={details}/>
+       </Col>
+        
+    </Row>
+
+
+    </Container>
   );
 }
 
